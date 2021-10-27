ae07bf9d931c2fe298da902d4d4810a1adcbda5b
UmberBn <humberto-bandeira@hotmail.com>
2021-10-27 00:02:33 -0400

    cards/listagem de transaçoes, cards de destaque

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+import HightligthCard from '../../components/HightligthCard';
+import TransactionCard, { TransactionCardProps } from '../../components/TransactionCard';
 
 import { 
   Container,
@@ -10,9 +13,52 @@ import {
   UserGreeting,
   UserName,
   Icon,
+  HightligthCards,
+  Transactions,
+  Title,
+  TransactionList,
 } from './styles';
 
+export interface DataListProps extends TransactionCardProps {
+  id: number;
+}
+
 const Dashboard: React.FC = () => {
+  const data: DataListProps[] = [
+    {
+      id: 1,
+      title: "Desenvolvimento de site",
+      type: 'positive',
+      amount: "R$ 12.000,00",
+      category: {
+        name: "Vendas",
+        icon: "dollar-sign",
+      },
+      date: "13/04/2020",
+    },
+    {
+      id: 2,
+      title: "Alimentação",
+      type: 'negative',
+      amount: "R$ 2.000,00",
+      category: {
+        name: "Alimentação",
+        icon: "dollar-sign",
+      },
+      date: "13/04/2020",
+    },
+    {
+      id: 3,
+      title: "Aluguel",
+      type: 'positive',
+      amount: "R$ 12.000,00",
+      category: {
+        name: "Casa",
+        icon: "dollar-sign",
+      },
+      date: "13/04/2020",
+    }
+  ]
   return (
     <Container>
       <Header>
@@ -33,6 +79,36 @@ const Dashboard: React.FC = () => {
           <Icon name="power"/>
         </UserWrapper>
       </Header>
+      <HightligthCards>
+        <HightligthCard
+          title='Entradas'
+          type='up'
+          amount={'R$ 17.400,00'}
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HightligthCard 
+          title='Saídas'
+          type="down"
+          amount={'R$ 17.400,00'}
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HightligthCard 
+          title='Total'
+          type="total"
+          amount='R$ 17.400,00'
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+      </HightligthCards>
+      <Transactions>
+        <Title>Listagem</Title>
+        <TransactionList
+          data={data}
+          keyExtractor={(item) => `${item.id}`}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{paddingBottom: getBottomSpace() || 20}}
+        />
+      </Transactions>
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,6 +1,9 @@
 import styled from 'styled-components/native'
 import { Feather } from '@expo/vector-icons'
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+import { FlatList } from 'react-native'
+import { DataListProps } from '.'
 
 export const Container = styled.View`
   flex: 1;
@@ -11,38 +14,35 @@ export const Header = styled.View`
   height: ${RFPercentage(42)}px;
   width: 100%;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   flex-direction: row;
 
 `
 export const UserWrapper = styled.View`
   width: 100%;
   padding: 0 24px;
+  margin-top: ${getStatusBarHeight() + RFValue(28)}px;
   flex-direction: row;
   justify-content: space-between;
+  align-items: center;
 `
-
 export const UserInfo = styled.View`
   flex-direction: row;
   align-items: center;
 `
-
 export const Photo = styled.Image`
   width: ${RFValue(48)}px;
   height: ${RFValue(48)}px;
   border-radius: ${RFValue(24)}px;
 `
-
 export const User = styled.View`
   margin-left: 17px;
 `
-
 export const UserGreeting = styled.Text`
   font-size: ${RFValue(18)}px;
   font-family: ${({theme}) => theme.fonts.regular};
   color: ${({theme}) => theme.colors.shape};
 `
-
 export const UserName = styled.Text`
   font-size: ${RFValue(18)}px;
   font-family: ${({theme}) => theme.fonts.bold};
@@ -52,3 +52,25 @@ export const Icon = styled(Feather)`
   color: ${({theme}) => theme.colors.secondary};
   font-size: ${RFValue(24)}px;
 `
+export const HightligthCards = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: { paddingHorizontal: 24 },
+})`
+  position: absolute;
+  margin-top: ${RFPercentage(20)}px;
+`
+export const Transactions = styled.View`
+  flex: 1;
+  padding: 0 24px;
+  margin-top: ${RFPercentage(12)}px;
+`
+
+export const Title = styled.Text`
+  font-size: ${RFValue(18)}px;
+  font-family: ${({theme}) => theme.fonts.regular};
+  margin-bottom: 16px;
+`
+export const TransactionList = styled(
+  FlatList as new () => FlatList<DataListProps>
+  )``
